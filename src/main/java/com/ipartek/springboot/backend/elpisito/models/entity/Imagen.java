b6bf888fd56e79265c46d979c03ddd612730f46c
SageshiSai <saikonogameryt@gmail.com>
2025-03-12 11:34:23 +0100

    atributos entidad imagen

--- src/main/java/com/ipartek/springboot/backend/elpisito/models/entity/Imagen.java
+++ src/main/java/com/ipartek/springboot/backend/elpisito/models/entity/Imagen.java
@@ -1,6 +1,29 @@
 package com.ipartek.springboot.backend.elpisito.models.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.io.Serial;
 import java.io.Serializable;
 
 public class Imagen implements Serializable {
+
+    @Serial
+	private static final long serialVersionUID = 7025742140603943281L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column Integer activo;
+
+    public Integer getActivo() {
+        return activo;
+    }
 }
