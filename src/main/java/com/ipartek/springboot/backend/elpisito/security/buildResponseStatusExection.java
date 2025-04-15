782ec511bc69250d5deebb6eeb577145adf428f4
SageshiSai <saikonogameryt@gmail.com>
2025-04-15 13:57:17 +0200

    ni idea, ya lo corregiré

--- src/main/java/com/ipartek/springboot/backend/elpisito/security/buildResponseStatusExection.java
+++
@@ -1,7 +0,0 @@
-package com.ipartek.springboot.backend.elpisito.security;
-
-public class buildResponseStatusExection extends RuntimeException {
-    public buildResponseStatusExection(String message) {
-        super(message);
-    }
-}
